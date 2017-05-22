8871731c93ace698b3bb91763a2cf53e4321574a
hhhhappy <olivier_zhu@163.com>
2017-05-22 14:18:19 +0200

    add csv file of languages

--- src/InsertExample.sql
+++ src/InsertExample.sql
@@ -3,6 +3,7 @@ start transaction;
 LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/nationnalite.csv' 
 INTO TABLE yomate.nationnalite FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
-
+LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/langue.csv' 
+INTO TABLE yomate.language FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 commit;
