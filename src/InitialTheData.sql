8a3374ce0b6d7723b8b633f614f2e8fc62a3fba7
hhhhappy <olivier_zhu@163.com>
2017-05-22 15:15:25 +0200

    correct the csv files and add the initialisation sql file

--- src/InitialTheData.sql
+++ src/InitialTheData.sql
@@ -1,9 +1,20 @@
 start transaction;
 
+delete from yomate.nationnalite;
+delete from yomate.language;
+delete from yomate.loisir;
+delete from yomate.profession;
+
 LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/nationnalite.csv' 
 INTO TABLE yomate.nationnalite FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/langue.csv' 
 INTO TABLE yomate.language FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/loisir.csv' 
+INTO TABLE yomate.loisir FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
+
+LOAD DATA LOCAL INFILE 'C:/Users/lenovo/Documents/GitHub/YomateFront/Yomate/src/profession.csv' 
+INTO TABLE yomate.profession FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
 
 commit;
--- src/json/util/JsonHelper.java
+++ src/json/util/JsonHelper.java
@@ -3,7 +3,31 @@ package json.util;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JsonHelper {
-	
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class JsonHelper {
+	public String serilisation(Object object){
+		ObjectMapper mapper = new ObjectMapper();
+		String s = null;
+		try {
+			s = mapper.writeValueAsString(object);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return s;
+	}
+
+	public Map<String, Object> deserilisation(String s){
+		Map<String, Object> map = null;
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			map = mapper.readValue(s, Map.class);
+			System.out.println("action: " + map.get("action"));
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return map;
+	}
 }
