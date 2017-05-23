c5ab16da608b3c50342b19e7dfac8fcf66147bd5
hhhhappy <olivier_zhu@163.com>
2017-05-23 17:15:14 +0200

    update the sql request

--- src/sql/util/SqlRequest.java
+++ src/sql/util/SqlRequest.java
@@ -1,7 +1,7 @@
 package sql.util;
 
 public class SqlRequest {
-	final static String SELECT_ALL_PROFIL = "SELECT temp.*, GROUP_CONCAT(la.language SEPARATOR ',') as language "
+	public final static String SELECT_ALL_PROFIL = "SELECT temp.*, GROUP_CONCAT(la.language SEPARATOR ',') as language "
 			+ " FROM (SELECT u.idUser, u.nom, u.prenom, u.birthday, u.sex, u.email, u.haspet, "
 			+ "u.situationFam, u.ecole, p.profession, n.nationnalite, GROUP_CONCAT(l.loisir SEPARATOR ',') as loisir "
 			+ "FROM YOMATE.USER as u, YOMATE.nationnalite as n, YOMATE.profession as p, "
@@ -12,7 +12,7 @@ public class SqlRequest {
 			+ "YOMATE.user_language as ula, YOMATE.language as la "
 			+ "WHERE temp.idUser = ula.user AND ula.language = la.id "
 			+ "GROUP BY temp.idUser;";
-	final static String SELECT_PROFIL_BY_ID = "SELECT temp.*, GROUP_CONCAT(la.language SEPARATOR ',') as language "
+	public final static String SELECT_PROFIL_BY_ID = "SELECT temp.*, GROUP_CONCAT(la.language SEPARATOR ',') as language "
 			+ " FROM (SELECT u.idUser, u.nom, u.prenom, u.birthday, u.sex, u.email, u.haspet, "
 			+ "u.situationFam, u.ecole, p.profession, n.nationnalite, GROUP_CONCAT(l.loisir SEPARATOR ',') as loisir "
 			+ "FROM YOMATE.USER as u, YOMATE.nationnalite as n, YOMATE.profession as p, "
@@ -23,6 +23,18 @@ public class SqlRequest {
 			+ "YOMATE.user_language as ula, YOMATE.language as la "
 			+ "WHERE temp.idUser = ula.user AND ula.language = la.id "
 			+ "GROUP BY temp.idUser;";
-	final static String CHECK_IF_USER_EXIST_BY_EMAIL_PW = "SELECT idUser FROM yomate.user "
+	public final static String CHECK_IF_USER_EXIST_BY_EMAIL_PW = "SELECT idUser FROM yomate.user "
 			+ "WHERE email= ? AND password= ?;";
+	public final static String CREATE_USER_COMPTE = "INSERT INTO yomate.user (nom, prenom, email, passward, sex) VALUES "
+			+ "(?, ?, ?, ?, ?);";
+	public final static String UPDATE_USER_PROFILE_MAIN_BY_ID = "UPDATE yomate.user SET nom = ?, prenom = ?, email = ?, passward = ?, "
+			+ "sex = ?, haspet = ?, situationFam = ?, ecole = ?, profession = ?, nationnalite = ? "
+			+ "WHERE idUser = ?;";
+	public final static String DELETE_USER_PROFILE_PHOTO_BY_ID = "DELETE FROM yomate.user_photo WHERE user = ?;";
+	public final static String UPDATE_USER_PROFILE_PHOTO = "INSERT INTO yomate.user_photo (user, image) values (?, ?);";
+	public final static String DELETE_USER_PROFILE_LOISIR_BY_ID = "DELETE FROM yomate.user_loisir WHERE user = ?;";
+	public final static String UPDATE_USER_PROFILE_LOISIR = "INSERT INTO yomate.user_loisir (user, loisir) values (?, ?);";
+	public final static String DELETE_USER_PROFILE_LANGUAGE_BY_ID = "DELETE FROM yomate.user_language WHERE user = ?;";
+	public final static String UPDATE_USER_PROFILE_LANGUAGE = "INSERT INTO yomate.user_language (user, language) values (?, ?);";
+	
 }
